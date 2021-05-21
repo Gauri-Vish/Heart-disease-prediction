19424773b56b4837414725b37bc13dbf74431d4b
gaurivish9920 <75183205+gaurivish9920@users.noreply.github.com>
2021-05-21 21:39:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"gauri1999at20@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
